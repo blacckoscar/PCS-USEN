702194b8149fdedccdb3698abbd095634ecf67e3
blacckoscar <okuakpabio0@gmail.com>
2023-07-20 01:20:20 +0100

    Oscars Commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 import Hero from "./Components/Hero";
-import Nav from "./component/Nav";
 import { Route, Routes } from "react-router-dom";
-import Shop from "./Components/Shop";
-import Explore from "./Components/Explore";
 import Cart from "./Components/Cart";
-import ProductDetails from "./Components/ProductDetails";
-import Collaborations from "./component/Collaborations";
-import AboutUs from "./component/About";
-import Order from "./Components/Order";
-import DeliveryPolicy from "./Components/DeliveryPolicy";
-import PrivacyPolicy from "./Components/PrivacyPolicy";
-import ProductPro from "./Components/ProductPro";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Services from "./Components/Services";
@@ -28,18 +18,6 @@ const Home = () => {
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/pro" element={<ProductPro />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route
-          path="/product-details/:productId"
-          element={<ProductDetails />}
-        />
-        <Route path="/collaborations" element={<Collaborations />} />
-        <Route path="/she" element={<Explore />} />
-        <Route path="/order" element={<Order />} />
-        <Route path="/delivery" element={<DeliveryPolicy />} />
-        <Route path="/policy" element={<PrivacyPolicy />} />
         <Route path='/about' element={<About  />} />
           <Route path='/contact' element={<Contact  />} />
           <Route path='/services' element={<Services  />} />
